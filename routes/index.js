33cf23929df094d5a496575722c93382922be93d
tayutaedomo <tayutaedomo@gmail.com>
2016-09-21 00:03:14 +0900

    Add /search

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/search', function(req, res, next) {
+  // Refer: http://qiita.com/itagakishintaro/items/265e4910a777b08a787c
+
+  var Twitter = require('twitter');
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  var options = {};
+  options.q = 'a';
+  options.count = 3;
+  client.get('search/tweets', options, function(error, tweets, response) {
+    res.send({
+      error: error,
+      tweets: tweets
+    });
+  });
+});
+
+
 module.exports = router;
+
